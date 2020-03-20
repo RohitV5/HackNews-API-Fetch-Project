9ad4551327c52c5bb8e964b4f9eae94f2ab65fca
Rohit Verma <verma.rohit.in@gmail.com>
2020-03-20 13:15:47 +0530

    fetching data from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  searchString: string;
+  resultList: any[];
+  pageSize = 10;
+  page = 1;
+
+
+  constructor(private http: HttpClient) {
+
+
+  }
+
+  getTitleAndAuthor() {
+    if (!this.searchString)
+      return;
+    this.http.get("http://hn.algolia.com/api/v1/search", {
+      params: {
+        query: this.searchString
+      },
+    })
+      .toPromise()
+      .then(response => {
+        console.log(response);
+        this.resultList = response["hits"];
+        this.resultList.forEach((element, index) => {
+          this.getSubmissionCount(element["author"], index)
+        })
+      })
+      .catch(console.log);
+  }
+
+  getSubmissionCount(author, index: number) {
+    if (!author)
+      return null;
+    this.http.get("http://hn.algolia.com/api/v1/users/" + author)
+      .toPromise()
+      .then(response => {
+
+        this.resultList[index].submissionCount = response["submission_count"];
+        this.resultList = JSON.parse(JSON.stringify(this.resultList))
+        console.log(this.resultList);
+      })
+      .catch(console.log);
+
+  }
+
+
+
 }
